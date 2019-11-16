9294994a492e99f96413394b4789622e75ecbfac
Simeon <sgarner96@gmail.com>
2019-11-16 04:00:11 -0700

    deleted conversation class and added ConvoAdapter

--- app/src/main/java/com/example/nuntius/MainActivity.java
+++ app/src/main/java/com/example/nuntius/MainActivity.java
@@ -5,13 +5,24 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static List<String> Conversation = new ArrayList<>();
+    public LinearLayoutManager linearLayoutManager;
+    public ConvoAdapter convoAdapter = new ConvoAdapter(Conversation);
+    public RecyclerView recyclerView;
+    public static final String MESSAGE = "MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +36,6 @@ public class MainActivity extends AppCompatActivity {
     public void message(View view){
         TextView textView = findViewById(R.id.message_text);
         String message = textView.toString();
-        Intent intent = new Intent();
 
     }
 
